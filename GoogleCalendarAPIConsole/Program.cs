05d91cca1e8632b70bd391e61fccb3de7e2fe046
Keen David Aguro <agurokeendavid@gmail.com>
2020-05-01 14:09:54 +0800

    Implemented displaying event method.

--- GoogleCalendarAPIConsole/Program.cs
+++ GoogleCalendarAPIConsole/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using System;
+using System.Linq;
 
+
+// Download Google.Apis.Calendar.v3 Package.
 namespace GoogleCalendarAPIConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintGoogleCalendarEvent();   
+        }
+
+        private static void PrintGoogleCalendarEvent()
+        {
+            // Initialize Calendar Service Object from Google.Apis.Services namespace;
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                // Input the api key that Google generate.
+                ApiKey = "YOUR_API_KEY",
+                ApplicationName = "Google Calendar API .NET Quickstart",
+
+            });
+            // Input the id of the calendar that you want to display.
+            var events = service.Events.List("CALENDAR_ID").Execute();
+
+            // Printing each event FROM Current Year to 2 Years after.
+            foreach (var myEvent in events.Items.OrderBy(o => o.Start.Date).ToList())
+            {
+                Console.WriteLine($"Event: {myEvent.Summary} Start: {myEvent.Start.Date}");
+            }
         }
     }
 }
